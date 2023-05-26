499adca7f21ab7b3c198e28245015e9c652b9609
masrur2022 <qurbonmamadovmasrur@gmail.com>
2023-05-26 23:16:47 +0500

    tst

--- evtvariables/var.go
+++ evtvariables/var.go
@@ -5,12 +5,12 @@ import (
 	"os"
 )
 
-// var IpUrl = os.Getenv("IpUrl")
-var IpUrl = "http://localhost:5173"
+var IpUrl = os.Getenv("IpUrl")
+// var IpUrl = "http://localhost:5173"
 // var IpUrl = "http://192.168.147.28:5173"
-// var DBUrl = os.Getenv("DBUrl")
+var DBUrl = os.Getenv("DBUrl")
 // var DBUrl = "mongodb://127.0.0.1:27017"
-var DBUrl = "mongodb://root:vQqOjqSsJ4@34.72.101.139:27017"
+// var DBUrl = "mongodb://root:vQqOjqSsJ4@34.72.101.139:27017"
 var Port = os.Getenv("PORT")
 
 func New()  {
